1e1d4ac1fa99fed8f10bb161450a6ba922e69ffd
AmlanAnupam-2007 <amlananupam2007@gmail.com>
2024-12-25 19:51:12 +0530

    hi

---
+++ Set.py
@@ -0,0 +1,16 @@
+a={1,2,2,3,4,4,"a","b",32.5} #unordered property of set= it places random elements at random places
+print(a)
+print(type(a))
+print(len(a))
+a.add("Banana")
+print(a)
+a.remove(4)
+print(a)
+
+b=set()
+print(type(b))
+b.add("Watermelon")
+print(b)
+
+
+
---
+++ Tuple.py
@@ -0,0 +1,18 @@
+a=(1,3,4,4,"Apple",34.2,True)
+
+print(a)
+print(len(a))
+print(type(a))
+print(a[-4])
+print(a[0])
+print(a[3:6])
+
+
+
+b=((1,3,4,4),[4,"Apple",34.2,True]) #Nested tupple (use of list as well as tupple within a tupple)
+
+print(b)
+print(b[1][2])
+
+
+
